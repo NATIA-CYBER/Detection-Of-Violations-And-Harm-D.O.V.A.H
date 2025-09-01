966f4bc8b983a46bd0695517818ec52b5724d558
natiautrugashvili <u_natia@proton.me>
2025-09-01 20:11:12 +0400

    Update db.py

--- src/common/db.py
+++ src/common/db.py
@@ -6,5 +6,7 @@ def get_engine() -> Engine:
     url = os.getenv("DATABASE_URL")
     if not url:
         raise RuntimeError("DATABASE_URL is not set")
-    # pre_ping avoids stale connections; 
     return create_engine(url, pool_pre_ping=True)
+
+
+
